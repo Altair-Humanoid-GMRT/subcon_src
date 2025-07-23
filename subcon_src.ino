d51629d03a41716ba0591011406a9e0363345028
lexciese <falah.naufal.zk@gmail.com>
2025-07-23 09:58:36 +0700

    set data_rate

--- subcon_src.ino
+++ subcon_src.ino
@@ -4,6 +4,10 @@
 ButtonActionControl* button = new ButtonActionControl();
 IMUControl* imu_controller  = new IMUControl();
 
+// CONFIGURATION (Change this based on motion program rate/hz, ask motion programmer)
+int data_rate = 100; // Set data rate
+int period = 1000 / data_rate;
+
 void setup(){
   /* start serial */
   Serial.begin(115200);
@@ -19,8 +23,16 @@ void setup(){
 
 void loop(){
   /* repeat buttonService and imuService */
-  Serial.print("$");
-  imu_controller->imuService();
-  button -> buttonService();
-  Serial.println();
+  static unsigned long lastTime = 0;
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastTime >= period) {
+    /* repeat buttonService and imuService */
+    Serial.print("$");
+    imu_controller->imuService();
+    button->buttonService();
+    Serial.println();
+
+    lastTime = currentTime;
+  }
 }
