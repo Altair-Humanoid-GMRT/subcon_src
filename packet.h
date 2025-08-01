42a23d2ae0611cbb68002be35d1d1de98233d68b
lexciese <falah.naufal.zk@gmail.com>
2025-08-01 20:35:30 +0700

    use attribute packed in SensorPacket struct so that the compiler not add padding between struct element

---
+++ packet.h
@@ -0,0 +1,12 @@
+struct __attribute__((packed)) SensorPacket {
+  float roll;    // x-axis orientation
+  float pitch;   // y-axis orientation
+  float yaw;     // z-axis orientation
+  float gyroX;
+  float gyroY;
+  float gyroZ;
+  float accelX;
+  float accelY;
+  float accelZ;
+  float buttonState;
+};
